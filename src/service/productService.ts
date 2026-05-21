22b48e62ae5411ce31cb23743998ebbbfd4fe32c
Shifa <shifa.appgallery@gmail.com>
2026-05-21 15:46:54 +0530

    changes in shop

--- src/service/productService.ts
+++ src/service/productService.ts
@@ -78,6 +78,9 @@ export const getProducts = async () => {
       "category",
       "gender",
       "variants",
+      "variants.variantImages",
+      "variants.color",
+      "variants.size",
       "media",
     ],
     where: {
